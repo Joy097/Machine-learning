1856130bd3b080869e50e01c1c3c6de2b1f1294d
Joy097 <shihabrumeechowdhury@gmail.com>
2023-03-25 16:06:22 +0600

    update 10

--- DC-GAN.py
+++ DC-GAN.py
@@ -5,13 +5,13 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 
-from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img
-from tensorflow.keras.preprocessing.image import img_to_array, array_to_img 
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, SeparableConv2D, MaxPooling2D, Lambda
-from tensorflow.keras.layers import Flatten, Dense, Dropout, BatchNormalization
-from tensorflow.keras.layers import Conv2DTranspose, Conv2D, add, concatenate
-from tensorflow.keras.layers import LeakyReLU, Activation, Reshape
-from tensorflow.keras.utils import plot_model
-from tensorflow.keras.optimizers import Adam 
-from tensorflow.keras.callbacks import ModelCheckpoint,ReduceLROnPlateau
+from keras.preprocessing.image import ImageDataGenerator, load_img
+from keras.preprocessing.image import img_to_array, array_to_img 
+from keras.models import Model
+from keras.layers import Input, SeparableConv2D, MaxPooling2D, Lambda
+from keras.layers import Flatten, Dense, Dropout, BatchNormalization
+from keras.layers import Conv2DTranspose, Conv2D, add, concatenate
+from keras.layers import LeakyReLU, Activation, Reshape
+from keras.utils import plot_model
+from keras.optimizers import Adam 
+from keras.callbacks import ModelCheckpoint,ReduceLROnPlateau
